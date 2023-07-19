690485f504ae689f0a44b12d875b3497aad398ef
Abhishekgaded <gadedabhishek01@gmail.com>
2023-07-19 21:44:12 +0530

    DetectCollision Script Added
     Added the Rigid Body
     Added the Box Collider

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,21 +9,33 @@ public class SpawnManager : MonoBehaviour
     public int AnimalIndex;
     private float SpawnRangeX = 20;
     private float SpawnPosZ = 20;
+    private float StartDelay = 2;
+    private float SpawnInterval = 1.5f;
+
+
     void Start()
     {
-        
+        InvokeRepeating("RandomSpawnAnimals",StartDelay,SpawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 SpawnPos = new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnPosZ);
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            AnimalIndex = Random.Range(0,animalPrefabs.Length);
-            Instantiate(animalPrefabs[AnimalIndex], SpawnPos, animalPrefabs[AnimalIndex].transform.rotation);
 
-        }
-
+        
     }
+
+    void RandomSpawnAnimals()
+    {
+        Vector3 SpawnPos = new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnPosZ);
+        AnimalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[AnimalIndex], SpawnPos, animalPrefabs[AnimalIndex].transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+
 }
